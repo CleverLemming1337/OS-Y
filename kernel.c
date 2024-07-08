805d9d5f6e5ba317db96246fbbd32bc6f75066bb
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-07-08 14:49:41 +0000

    Add version main@0.1.0

--- kernel.c
+++ kernel.c
@@ -1,5 +1,7 @@
 #include <efi.h>
 #include <efilib.h>
+#define VERSION "0.1.0"
+#define LVERSION L"0.1.0"
 
 EFI_STATUS
 EFIAPI
@@ -7,5 +9,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
+  Print(LVERSION);
+  while(1);
   return EFI_SUCCESS;
 }
