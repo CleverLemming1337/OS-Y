01c35e24ca085139ab6b9d4f4b7e1a016a80de08
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-07-08 15:19:20 +0000

    Update Kernel

--- compile_kernel.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -Iinc -fpic -ffreestanding -fno-stack-protector -fno-stack-check -fshort-wchar -mno-red-zone -maccumulate-outgoing-args -c kernel.c -o kernel.o
--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
 #include <efi.h>
 #include <efilib.h>
 #define VERSION "0.1.0"
-#define LVERSION L"0.1.0"
+#define LVERSION L"0.1.0\n"
 
 EFI_STATUS
 EFIAPI
@@ -10,7 +10,6 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
   Print(LVERSION);
-  EFI_INPUT_KEY Key;
-  while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_NOT_READY);
+  
   return EFI_SUCCESS;
 }
