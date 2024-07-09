293f784bd295270e383a5268f2b13dd8b5d4c0d8
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-07-09 12:18:29 +0200

    Add keyboard

--- kernel.c
+++ kernel.c
@@ -1,7 +1,7 @@
 #include <efi.h>
 #include <efilib.h>
-#define VERSION "0.1.0"
-#define LVERSION L"0.1.0\n"
+#define VERSION "0.1.1"
+#define LVERSION L"0.1.1\n"
 
 EFI_STATUS
 EFIAPI
@@ -10,6 +10,11 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
   Print(LVERSION);
-  
+
+  Print(L"\nPress any key to exit");
+  EFI_INPUT_KEY Key;
+  while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_NOT_READY);
+  Print(L"Exit");
+
   return EFI_SUCCESS;
 }
