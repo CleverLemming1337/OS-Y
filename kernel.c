32c154c29efb2142a9b437884f41e250cc2ac7f9
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-07-09 10:26:58 +0000

    Show keyboard input main@0.1.2

--- kernel.c
+++ kernel.c
@@ -1,19 +1,29 @@
 #include <efi.h>
 #include <efilib.h>
-#define VERSION "0.1.1"
-#define LVERSION L"0.1.1\n"
+#define VERSION "0.1.2"
+#define LVERSION L"0.1.2\n"
 
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
+  EFI_INPUT_KEY Key;
+  EFI_STATUS Status;
+
   Print(L"Hello, world!\n");
   Print(LVERSION);
 
   Print(L"\nPress any key to exit");
-  EFI_INPUT_KEY Key;
-  while (SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key) == EFI_NOT_READY);
+
+  while (1) {
+        // Warte auf Tastatureingabe
+        Status = uefi_call_wrapper(SystemTable->ConIn->ReadKeyStroke, 2, SystemTable->ConIn, &Key);
+        if (Status == EFI_SUCCESS) {
+            // Gebe das Zeichen auf dem Bildschirm aus
+            Print(L"%c", Key.UnicodeChar);
+        }
+    }
   Print(L"Exit");
 
   return EFI_SUCCESS;
